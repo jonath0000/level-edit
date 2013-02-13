63b46f6759ff840fd1f9742e5f7b5297d8c2125d
jonath0000 <nonis.b@gmail.com>
2013-02-13 10:36:53 +0100

    Add LevelFileInterface to levelmodel package.

---
+++ src/levelmodel/LevelFileInterface.java
@@ -0,0 +1,22 @@
+package levelmodel;
+
+
+/**
+ * Abstract level file class to represent any I/O format. 
+ */
+public interface LevelFileInterface {
+        
+    /**
+     * Write level to file.
+     * 
+     * @return true if ok.
+     */
+    public boolean write(DummyObjectList dummyObjects, TileMap tileMap);
+    
+    /**
+     * Read a level into memory.
+     * @return true if ok.
+     */
+    public boolean read(DummyObjectList dummyObjects, DummyObject [] dummyTypes, 
+            TileMap tileMap);
+}
