11cb34b98f59812fee3c10430691ecd58a28524d
Jonath <nonis.b@gmail.com>
2014-11-30 18:46:53 +0100

    Add undo to dummy moving.

--- src/tools/SelectDummyTool.java
+++ src/tools/SelectDummyTool.java
@@ -6,22 +6,27 @@ import levelmodel.Level;
 
 public class SelectDummyTool implements Tool {
 
+	DummyObject movedDummy = null;
 	
 	@Override
 	public void click(int worldX, int worldY, int tileX, int tileY,
 			boolean repeated, Level currentLevel, DummyObjectFactory dummyObjectFactory, 
 			TileSelector tileSelector, int currentTileLayer) {
 		if (repeated) {
-			DummyObject clickedDummy = currentLevel.getDummyObjects().getSelected();
-			if (clickedDummy != null) {
-				if (!(worldX < clickedDummy.x || worldX > clickedDummy.x + clickedDummy.w
-						|| worldY < clickedDummy.y || worldY > clickedDummy.y + clickedDummy.h)) {
-					clickedDummy.x = worldX - clickedDummy.w/2;
-					clickedDummy.y = worldY - clickedDummy.h/2;
+			if (movedDummy == null) {
+				currentLevel.isAboutToAlterState();
+			}
+			movedDummy = currentLevel.getDummyObjects().getSelected();
+			if (movedDummy != null) {
+				if (!(worldX < movedDummy.x || worldX > movedDummy.x + movedDummy.w
+						|| worldY < movedDummy.y || worldY > movedDummy.y + movedDummy.h)) {
+					movedDummy.x = worldX - movedDummy.w/2;
+					movedDummy.y = worldY - movedDummy.h/2;
 				}
 			}
 		} else {
 			currentLevel.getDummyObjects().selectDummy(worldX, worldY);
+			movedDummy = null;
 		}
 	}
 
